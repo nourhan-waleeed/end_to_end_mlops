4e28aa74b87f74242a99e160c6a3c36d81e4e8db
nourhan-waleeed <nourhanw21@gmail.com>
2023-11-03 08:29:01 +0200

    folder structure added

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -11,7 +11,7 @@ from typing import Any
 
 
 
-@ensure_annotations
+#@ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
 
@@ -30,14 +30,14 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
-    except BoxValueError:
+    #except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
     
 
 
-@ensure_annotations
+#@ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
 
@@ -51,7 +51,7 @@ def create_directories(path_to_directories: list, verbose=True):
             logger.info(f"created directory at: {path}")
 
 
-@ensure_annotations
+#@ensure_annotations
 def save_json(path: Path, data: dict):
     """save json data
 
@@ -67,7 +67,7 @@ def save_json(path: Path, data: dict):
 
 
 
-@ensure_annotations
+#@ensure_annotations
 def load_json(path: Path) -> ConfigBox:
     """load json files data
 
@@ -84,7 +84,7 @@ def load_json(path: Path) -> ConfigBox:
     return ConfigBox(content)
 
 
-@ensure_annotations
+#@ensure_annotations
 def save_bin(data: Any, path: Path):
     """save binary file
 
@@ -96,7 +96,7 @@ def save_bin(data: Any, path: Path):
     logger.info(f"binary file saved at: {path}")
 
 
-@ensure_annotations
+#@ensure_annotations
 def load_bin(path: Path) -> Any:
     """load binary data
 
@@ -112,7 +112,7 @@ def load_bin(path: Path) -> Any:
 
 
 
-@ensure_annotations
+#@ensure_annotations
 def get_size(path: Path) -> str:
     """get size in KB
 
